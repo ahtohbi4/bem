cda43496a039302c9c9e8a41eeefc5552574fb47
Alexander Antonov <aanodov@gmail.com>
2013-04-20 19:55:35 +0400

    add log

--- bem-create.py
+++ bem-create.py
@@ -1,28 +1,35 @@
 import sublime, sublime_plugin
 import os, os.path
+import datetime
 import re
-import functools # remove after refactoring
 import webbrowser
 
 S = sublime.load_settings("BEM.sublime-settings")
 
 
 
-# @method log_this Output debug information in file debug
-#
-# @param String 'line' Output string
-def log_this(line):
-    #S.set("log_path", os.path.join(sublime.installed_packages_path(), u"BEM/log"))
-    #sublime.save_settings("BEM.sublime-settings")
+# @class Scribe Log information
+class Scribe():
+    def __init__(self):
+        # installed_packages_path()
+        if os.path.exists(os.path.join(sublime.packages_path(), "BEM")):
+            logPath = os.path.join(sublime.packages_path(), "BEM")
 
-    f = open(os.path.join(sublime.packages_path(), "BEM", "log"), 'w')
-    f.write(line)
-    #sublime.message_dialog(f.read())
-    f.close()
+            if not os.path.exists(os.path.join(logPath, "log")):
+                create_file(logPath, "log")
+
+        self.logFile = open(os.path.join(os.path.join(logPath, "log")), 'a')
+
+    def log(self, line):
+        self.logFile.write(datetime.datetime.now().isoformat(' ') + "  " + line + "\n")
+
+    def stop(self):
+        self.logFile.write("\n")
+        self.logFile.close()
 
 
 
-# @method create_file Create a files
+# @method create_file Create a files and log it
 #
 # @param String 'path' Path to file dir
 # @param String 'name' File name with extantion (test.html)
@@ -32,8 +39,30 @@ def create_file(path, name, content=''):
         file = open(os.path.join(path, name), 'w')
         file.write(content)
         file.close()
+
+        return "NOTICE: File '%s' was created in '%s'." % (name, path)
+
     except:
         sublime.status_message("Unable to create file " + name)
+        
+        return "FAIL: Unable to create file '%s' in '%s'." % (name, path)
+
+
+
+# @method create_dir Create a dir and log it
+#
+# @param String 'path' Path to file dir
+# @param String 'name' File name with extantion (test.html)
+def create_dir(path, name):
+    try:
+        os.mkdir(os.path.join(path, name))
+
+        return "NOTICE: Folder '%s' was created in '%s'." % (name, path)
+
+    except:
+        sublime.status_message("Unable to create file " + name)
+
+        return "FAIL: Unable to create file '%s' in '%s'." % (block + TECH[t]['filetype'], path)
 
 
 
@@ -43,10 +72,14 @@ def create_file(path, name, content=''):
 # BLOCK__ELEM_MOD_VAL
 class BemCreateCommand(sublime_plugin.TextCommand):
     def run(self, view):
+        self.Scribe = Scribe()
+
         self.view.window().show_input_panel("BEM Create:", "", self.get_command, None, None)
 
     # Generate command object
     def get_command(self, command_line):
+        self.Scribe.log("NOTICE: Command '%s' was received." % command_line)
+
         if re.match('^[a-z0-9-]+$', command_line, flags=re.IGNORECASE):
             unit_type = 'block'
 
@@ -68,6 +101,8 @@ class BemCreateCommand(sublime_plugin.TextCommand):
         }
 
         if unit_type:
+            self.Scribe.log("NOTICE: Trying to create %s." % unit_type)
+
             command['unit_type'] = unit_type
 
             # Split command_line on Block and Elem part
@@ -102,6 +137,8 @@ class BemCreateCommand(sublime_plugin.TextCommand):
 
         else:
             sublime.message_dialog("Error: Incorrect command format.\n\nLearn more at https://github.com/ahtohbi4/bem#readme")
+            self.Scribe.log("FAIL: Incorrect command format. Learn more information on page https://github.com/ahtohbi4/bem#readme\n")
+            self.Scribe.stop()
 
     # Generate files
     def generate_files(self, command):
@@ -118,16 +155,18 @@ class BemCreateCommand(sublime_plugin.TextCommand):
         # BLOCK
         if command['unit_type'] == 'block':
             if not os.path.exists(blockPath):
-                os.mkdir(blockPath)
+                self.Scribe.log(create_dir(BLOCKS_PATH, block))
 
                 for t in TECH_FILES_CONTENT['block']:
-                    content = TECH_FILES_CONTENT['block'][t].format(block = block)
-                    create_file(blockPath, block + TECH[t]['filetype'], content)
+                    content = TECH_FILES_CONTENT['block'][t].format(block = block, dep = "")
+                    self.Scribe.log(create_file(blockPath, block + TECH[t]['filetype'], content))
 
                 message = message + "\n\nBlock '%s' was created." % block
+                self.Scribe.log("NOTICE: Block '%s' was created." % block)
 
             else:
                 error_message = "Alert!\n\nBlock '%s' already exists." % block
+                self.Scribe.log("FAIL: Block '%s' already exists." % block)
 
         # MOD
         elif command['unit_type'] == 'mod':
@@ -135,48 +174,54 @@ class BemCreateCommand(sublime_plugin.TextCommand):
             value = command['units']['mod'].values()[0]
 
             if not os.path.exists(blockPath):
-                os.mkdir(blockPath)
+                self.Scribe.log(create_dir(BLOCKS_PATH, block))
+
                 dep_content = TECH_FILES_CONTENT['block']['deps'].format(dep = "\n        {\n            mods: { " + mod + ": '" + value + "' }\n        }\n    ")
-                create_file(blockPath, block + TECH['deps']['filetype'], dep_content)
+                self.Scribe.log(create_file(blockPath, block + TECH['deps']['filetype'], dep_content))
                 self.view.window().open_file(os.path.join(blockPath, block + TECH['deps']['filetype']), sublime.TRANSIENT)
 
             modPath = os.path.join(blockPath, "_" + mod)
 
             if not os.path.exists(modPath):
-                os.mkdir(modPath)
+                self.Scribe.log(create_dir(blockPath, "_" + mod))
 
                 for t in TECH_FILES_CONTENT['mod']:
                     content = TECH_FILES_CONTENT['mod'][t].format(block = block, mod = mod, value = value)
-                    create_file(modPath, block + "_" + mod + "_" + value + TECH[t]['filetype'], content)
+                    self.Scribe.log(create_file(modPath, block + "_" + mod + "_" + value + TECH[t]['filetype'], content))
 
                 message = message + "\n\nMod '%s' with Value '%s' for Block '%s' was created." % (mod, value, block)
+                self.Scribe.log("NOTICE: Mod '%s' with Value '%s' for Block '%s' was created." % block)
 
             else:
                 error_message = "Alert!\n\nMod '%s' for Block '%s' already exists." % (mod, block)
+                self.Scribe.log("FAIL: Mod '%s' with Value '%s' for Block '%s' already exists." % (mod, value, block))
 
         # ELEM
         elif command['unit_type'] == 'elem':
             elem = command['units']['elem']
 
             if not os.path.exists(blockPath):
-                os.mkdir(blockPath)
+                self.Scribe.log(create_dir(BLOCKS_PATH, block))
+
                 dep_content = TECH_FILES_CONTENT['block']['deps'].format(dep = "\n        { elem: '" + elem + "' }\n    ")
-                create_file(blockPath, block + TECH['deps']['filetype'], dep_content)
+                self.Scribe.log(create_file(blockPath, block + TECH['deps']['filetype'], dep_content))
                 self.view.window().open_file(os.path.join(blockPath, block + TECH['deps']['filetype']), sublime.TRANSIENT)
 
             elemPath = os.path.join(blockPath, "__" + elem)
 
             if not os.path.exists(elemPath):
-                os.mkdir(elemPath)
+                self.Scribe.log(create_dir(blockPath, "__" + elem))
 
                 for t in TECH_FILES_CONTENT['elem']:
                     content = TECH_FILES_CONTENT['elem'][t].format(block = block, elem = elem)
-                    create_file(elemPath, block + "__" + elem + TECH[t]['filetype'], content)
+                    self.Scribe.log(create_file(elemPath, block + "__" + elem + TECH[t]['filetype'], content))
 
                 message = message + "\n\nElem '%s' for Block '%s' was created." % (elem, block)
+                self.Scribe.log("NOTICE: Elem '%s' for Block '%s' was created." % (elem, block))
 
             else:
                 error_message = "Alert!" + "\n\nElem '%s' for Block '%s' already exists." % (elem, block)
+                self.Scribe.log("FAIL: Elem '%s' for Block '%s' already exists." % (elem, block))
 
         # ELEMMOD
         elif command['unit_type'] == 'elemMod':
@@ -185,29 +230,32 @@ class BemCreateCommand(sublime_plugin.TextCommand):
             value = command['units']['elemMod'].values()[0]
 
             if not os.path.exists(blockPath):
-                os.mkdir(blockPath)
+                self.Scribe.log(create_dir(BLOCKS_PATH, block))
+
                 dep_content = TECH_FILES_CONTENT['block']['deps'].format(dep = "\n        {\n            elem: '" + elem + "',\n            mods: { " + elemMod + ": '" + value + "' }\n        }\n    ")
-                create_file(blockPath, block + TECH['deps']['filetype'], dep_content)
+                self.Scribe.log(create_file(blockPath, block + TECH['deps']['filetype'], dep_content))
                 self.view.window().open_file(os.path.join(blockPath, block + TECH['deps']['filetype']), sublime.TRANSIENT)
 
             elemPath = os.path.join(blockPath, "__" + elem)
 
             if not os.path.exists(elemPath):
-                os.mkdir(elemPath)
+                self.Scribe.log(create_dir(blockPath, "__" + elem))
 
             elemModPath = os.path.join(elemPath, "_" + elemMod)
 
             if not os.path.exists(elemModPath):
-                os.mkdir(elemModPath)
+                self.Scribe.log(create_dir(elemPath, "_" + elemMod))
 
                 for t in TECH_FILES_CONTENT['elemMod']:
                     content = TECH_FILES_CONTENT['elemMod'][t].format(block = block, elem = elem, elemMod = elemMod, value = value)
-                    create_file(elemModPath, block + "__" + elem + "_" + elemMod + "_" + value + TECH[t]['filetype'], content)
+                    self.Scribe.log(create_file(elemModPath, block + "__" + elem + "_" + elemMod + "_" + value + TECH[t]['filetype'], content))
 
                 message = message + "\n\nMod '%s' with Value '%s' for Elem '%s' in Block '%s' was created." % (elemMod, value, elem, block)
+                self.Scribe.log("NOTICE: Mod '%s' with Value '%s' for Elem '%s' in Block '%s' was created." % (elemMod, value, elem, block))
 
             else:
-                error_message = "Alert!" + "\n\nMod '%s' for Elem '%s' in Block '%s' was created." % (elemMod, elem, block)
+                error_message = "Alert!" + "\n\nMod '%s' with Value '%s' for Elem '%s' in Block '%s' already exists." % (elemMod, value, elem, block)
+                self.Scribe.log("ERROR: Mod '%s' with Value '%s' for Elem '%s' in Block '%s' already exists." % (elemMod, value, elem, block))
 
         # Messages and refresh dir
         if error_message != "":
@@ -216,6 +264,8 @@ class BemCreateCommand(sublime_plugin.TextCommand):
         else:
             self.view.window().run_command("refresh_folder_list")
             sublime.message_dialog(message)
+
+        self.Scribe.stop()
 
 
 
